13a745da41224e45649a15de5aba0b5853f9ea6f
sleepd3aler <enjoydeathcore228@gmail.com>
2026-01-07 12:28:28 +0300

    44.7 Преобразование даты

--- src/main/java/ru/grabber/utils/HabrDateTimeParser.java
+++ src/main/java/ru/grabber/utils/HabrDateTimeParser.java
@@ -6,10 +6,6 @@ import java.time.format.DateTimeFormatter;
 public class HabrDateTimeParser implements DateTimeParser {
     @Override
     public LocalDateTime parse(String parse) {
-        if (parse.substring(19).equals("+")) {
-            String dt = parse.split("\\+")[0].trim();
-            return LocalDateTime.parse(dt, DateTimeFormatter.ISO_LOCAL_DATE_TIME);
-        }
         String dt = parse.substring(0, 19);
         return LocalDateTime.parse(dt, DateTimeFormatter.ISO_LOCAL_DATE_TIME);
     }
